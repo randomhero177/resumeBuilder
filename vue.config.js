cbb21872260d40b8764eeb880387c0ce736d1fa8
Konstantin Vasin <k.vasin@sape.ru>
2020-02-13 20:41:36 +0300

    add vuetify; change webpack config

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "transpileDependencies": [
+    "vuetify"
+  ],
+  devServer: {
+    port: 4322,
+    open: true,
+	overlay: {
+      warnings: false,
+      errors: false,
+    },
+  },
+}
