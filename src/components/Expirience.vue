68d8b4f92367d211b47806de9b3607c60e934ff2
Konstantin Vasin <k.vasin@sape.ru>
2020-02-26 18:03:12 +0300

    add vue-wysiwyg; beautify end/start Date;

--- src/components/Expirience.vue
+++ src/components/Expirience.vue
@@ -14,100 +14,110 @@
       </div>
       <div class="col-md-6">
         <div class="form__label">Start date</div>
-        <v-date-picker
-          v-model="dayStart"
-          type="month"
-          width="290"
-          class="mt-4"
-        />
+        <div class="form__picker">
+          <input type="text" class="input" v-model="dayStart">
+          <v-date-picker
+            v-model="dayStart"
+            type="month"
+            width="290"
+            class="form__picker-elem"
+          />
+        </div>
       </div>
       <div class="col-md-6">
         <div class="form__label">End date</div>
-        <v-date-picker
-          v-model="endDate"
-          type="month"
-          width="290"
-          class="mt-4"
-        />
+        <div class="form__picker" v-if="!isCurrentPosition">
+          <input type="text" class="input" v-model="endDate">
+          <v-date-picker
+            v-model="endDate"
+            type="month"
+            width="290"
+            class="form__picker-elem"
+          />
+        </div>
         <div>
-          <input type="checkbox" v-model="checkbox">
+          <input type="checkbox" v-model="isCurrentPosition">
           <label>Все еще работаю здесь</label>
-          {{ checkbox }}
         </div>
       </div>
-    </div>
-    <v-container fluid>
-      <v-combobox
-        v-model="model"
-        :filter="filter"
-        :hide-no-data="!search"
-        :items="items"
-        :search-input.sync="search"
-        hide-selected
-        label="Search for an option"
-        multiple
-        small-chips
-        solo
-      >
-        <template v-slot:no-data>
-          <v-list-item>
-            <span class="subheading">Create</span>
-            <v-chip
-              :color="`${colors[nonce - 1]} lighten-3`"
-              label
-              small
-            >
-              {{ search }}
-            </v-chip>
-          </v-list-item>
-        </template>
-        <template v-slot:selection="{ attrs, item, parent, selected }">
-          <v-chip
-            v-if="item === Object(item)"
-            v-bind="attrs"
-            :color="`${item.color} lighten-3`"
-            :input-value="selected"
-            label
-            small
-          >
-            <span class="pr-2">
-              {{ item.text }}
-            </span>
-            <unicon name="times-circle" fill="#1867c0" width="15px" @click="parent.selectItem(item)"/>
-          </v-chip>
-        </template>
-        <template v-slot:item="{ index, item }">
-          <v-text-field
-            v-if="editing === item"
-            v-model="editing.text"
-            autofocus
-            flat
-            background-color="transparent"
-            hide-details
+      <div class="col-md-12">
+        <div class="form__label">Key responsibilities and description:</div>
+        <wysiwyg v-model="myHTML" />
+      </div>
+      <div class="col-md-12">
+        <div class="form__label">Key skills:</div>
+          <v-combobox
+            v-model="model"
+            :filter="filter"
+            :hide-no-data="!search"
+            :items="items"
+            :search-input.sync="search"
+            hide-selected
+            label="Search for an option"
+            multiple
+            small-chips
             solo
-            @keyup.enter="edit(index, item)"
-          ></v-text-field>
-          <v-chip
-            v-else
-            :color="`${item.color} lighten-3`"
-            dark
-            label
-            small
           >
-            {{ item.text }}
-          </v-chip>
-          <v-spacer></v-spacer>
-          <v-list-item-action @click.stop>
-            <v-btn
-              icon
-              @click.stop.prevent="edit(index, item)"
-            >
-              <v-icon>{{ editing !== item ? 'mdi-pencil' : 'mdi-check' }}</v-icon>
-            </v-btn>
-          </v-list-item-action>
-        </template>
-      </v-combobox>
-    </v-container>
+            <template v-slot:no-data>
+              <v-list-item>
+                <span class="subheading">Create</span>
+                <v-chip
+                  :color="`${colors[nonce - 1]} lighten-3`"
+                  label
+                  small
+                >
+                  {{ search }}
+                </v-chip>
+              </v-list-item>
+            </template>
+            <template v-slot:selection="{ attrs, item, parent, selected }">
+              <v-chip
+                v-if="item === Object(item)"
+                v-bind="attrs"
+                :color="`${item.color} lighten-3`"
+                :input-value="selected"
+                label
+                small
+              >
+                <span class="pr-2">
+                  {{ item.text }}
+                </span>
+                <unicon name="times-circle" fill="#1867c0" width="15px" @click="parent.selectItem(item)"/>
+              </v-chip>
+            </template>
+            <template v-slot:item="{ index, item }">
+              <v-text-field
+                v-if="editing === item"
+                v-model="editing.text"
+                autofocus
+                flat
+                background-color="transparent"
+                hide-details
+                solo
+                @keyup.enter="edit(index, item)"
+              ></v-text-field>
+              <v-chip
+                v-else
+                :color="`${item.color} lighten-3`"
+                dark
+                label
+                small
+              >
+                {{ item.text }}
+              </v-chip>
+              <v-spacer></v-spacer>
+              <v-list-item-action @click.stop>
+                <v-btn
+                  icon
+                  @click.stop.prevent="edit(index, item)"
+                >
+                  <v-icon>{{ editing !== item ? 'mdi-pencil' : 'mdi-check' }}</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </template>
+          </v-combobox>
+      </div>
+    </div>
     <div v-if="workList.length">
       Key skills: native JS, Jquery, TypeScript, Git, Gulp, Less, MVS, .NET (as a stack
       with backend)
@@ -121,13 +131,12 @@
     name: 'Expirience',
 
     data: () => ({
+      myHTML: '',
       dayStart: new Date().toISOString().substr(0, 10),
       endDate: new Date().toISOString().substr(0, 10),
       workList: [],
-      activator: null,
-      attach: null,
       colors: ['green', 'purple', 'indigo', 'cyan', 'teal', 'orange'],
-      checkbox: true,
+      isCurrentPosition: false,
       editing: null,
       index: -1,
       items: [
@@ -142,7 +151,7 @@
         },
       ],
       nonce: 1,
-      menu: false,
+      menu: true,
       model: [
         {
           text: 'Foo',
@@ -156,10 +165,13 @@
     methods: {
       addNewPosition() {
         const config = {
+          myHTML: '',
           company: '',
           position: '',
           dayStart: new Date().toISOString().substr(0, 10),
-          dayEnd: new Date().toISOString().substr(0, 10),
+          endDate: new Date().toISOString().substr(0, 10),
+          isCurrentPosition: false,
+          editing: null,
         };
         this.workList.push(config);
       },
--- src/main.js
+++ src/main.js
@@ -3,19 +3,21 @@ import store from '@/store/index';
 import router from '@/router/index';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import 'vuetify/dist/vuetify.css'
-import '@mdi/font/css/materialdesignicons.css'
-import '@/assets/vuetify.css'
+import 'vuetify/dist/vuetify.css';
+import '@mdi/font/css/materialdesignicons.css';
+import '@/assets/vuetify.css';
 import '@/assets/common.styl';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import Unicon from 'vue-unicons'
-import { uniUserPlus, uniTimesCircle, uniCommentAltShield, uniCommentAltPlus, uniCommentPlus, uniCommentRedo, uniCommentUpload, uniCommentAltSearch, uniCommentAltUpload, uniCommentAltMedical, uniCommentMedical, uniCommentAltShare, uniCommentShare, uniCommentShield, uniCommentSearch, uniCommentQuestion } from 'vue-unicons/src/icons'
+import wysiwyg from "vue-wysiwyg";
+import "vue-wysiwyg/dist/vueWysiwyg.css";
+import Unicon from 'vue-unicons';
+import { uniUserPlus, uniTimesCircle, uniCommentAltShield, uniCommentAltPlus, uniCommentPlus, uniCommentRedo, uniCommentUpload, uniCommentAltSearch, uniCommentAltUpload, uniCommentAltMedical, uniCommentMedical, uniCommentAltShare, uniCommentShare, uniCommentShield, uniCommentSearch, uniCommentQuestion } from 'vue-unicons/src/icons';
 
-Unicon.add([uniUserPlus, uniTimesCircle, uniCommentAltShield, uniCommentAltPlus, uniCommentPlus, uniCommentRedo, uniCommentUpload, uniCommentAltSearch, uniCommentAltUpload, uniCommentAltMedical, uniCommentMedical, uniCommentAltShare, uniCommentShare, uniCommentShield, uniCommentSearch, uniCommentQuestion])
-Vue.use(Unicon)
-
+Unicon.add([uniUserPlus, uniTimesCircle, uniCommentAltShield, uniCommentAltPlus, uniCommentPlus, uniCommentRedo, uniCommentUpload, uniCommentAltSearch, uniCommentAltUpload, uniCommentAltMedical, uniCommentMedical, uniCommentAltShare, uniCommentShare, uniCommentShield, uniCommentSearch, uniCommentQuestion]);
+Vue.use(Unicon);
+Vue.use(wysiwyg, {});
 library.add(faUserSecret);
 
 
