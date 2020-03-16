2e1619c4f11a55172bf8417b476318f723252a6b
Konstantin Vasin <k.vasin@sape.ru>
2020-03-16 19:21:34 +0300

    small refactoring

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -47,10 +47,10 @@
         const tag = {
           name: newTag,
           code: newTag.substring(0, 2) + Math.floor((Math.random() * 10000000))
-        }
-        this.options.push(tag)
-        this.value.push(tag)
-      }
+        };
+        this.options.push(tag);
+        this.value.push(tag);
+      },
     },
   }
 </script>
