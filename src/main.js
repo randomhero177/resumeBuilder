88320107ac89d3beeafa7968af1c76290abf5683
Konstantin Vasin <k.vasin@sape.ru>
2020-03-26 04:27:56 +0300

    remove vuetify

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import Vue from 'vue';
 import store from '@/store/index';
 import router from '@/router/index';
 import App from './App.vue';
-import vuetify from './plugins/vuetify';
 import '@/assets/grid.min.css';
-import 'vuetify/dist/vuetify.css';
 import '@mdi/font/css/materialdesignicons.css';
-import '@/assets/vuetify.css';
 import '@/assets/common.styl';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
@@ -32,7 +29,6 @@ Vue.component('datepicker', Datepicker);
 Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
   store,
   router,
   render: h => h(App)
