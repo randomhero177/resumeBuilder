0f92d72e921237ef3c9deed7aaf47e2f824b1560
Konstantin Vasin <k.vasin@sape.ru>
2020-03-27 19:49:53 +0300

    add template system

---
+++ src/store/modules/template.js
@@ -0,0 +1,25 @@
+const state = {
+  templateName: 'template',
+  templateList: [],
+};
+
+const actions = {
+  setTemplateName({ commit }, templateName) {
+    commit('setTemplateName', templateName)
+  },
+};
+
+const mutations = {
+  setTemplateName(state, templateName) {
+    state.templateName = templateName;
+  },
+};
+
+const template = {
+  namespaced: true,
+  state,
+  actions,
+  mutations,
+};
+
+export default template;
