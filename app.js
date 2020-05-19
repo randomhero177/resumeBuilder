d6647e432eebc789d62361da909b5e82e4b84bf8
Konstantin Vasin <randomhero177@gmail.com>
2020-05-20 01:27:34 +0300

    connet to mongoDb

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('-appp')
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = config.get('port') || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    app.listen(PORT, () => console.log(`app started on port ${PORT}`))
+  } catch (e) {
+    console.log('Server error', e.message)
+    process.exit(1);
+  }
+}
+
+start()
+
