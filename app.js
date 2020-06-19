23a525349090c3cca50fb611d4dd03266a0052cc
Konstantin Vasin <randomhero177@gmail.com>
2020-06-19 17:37:42 +0300

    set production env

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
-
+const path = require('path')
 const app = express();
 
 
 app.use(express.json({ extended: true, limit: '50mb' }));
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/resume', require('./routes/resumes.routes'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'frontend', 'dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  })
+}
 
 const PORT = config.get('port') || 5000;
 
