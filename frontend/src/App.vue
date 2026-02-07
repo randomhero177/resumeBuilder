664100b9ce7dd9cb9f67b8a987e0dd1bead3e3ad
randomhero177 <randomhero177@gmail.com>
2026-02-07 23:20:28 +0100

    [#vue3]: add @vue/compat to vue.config

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,7 @@
 <template>
-  <div id="app">
     <layout-dynamic />
-  </div>
 </template>
 
-<script>
-import LayoutDynamic from '@/layouts/LayoutDynamic.vue';
-
-export default {
-  name: 'App',
-
-  components: {
-    LayoutDynamic,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+<script setup lang="ts">
+  import LayoutDynamic from '@/layouts/LayoutDynamic.vue';
 </script>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,9 +3,28 @@ module.exports = {
     port: 4322,
     open: true,
     proxy: 'http://localhost:5000',
-    overlay: {
-      warnings: false,
-      errors: false,
+    client: {
+      overlay: {
+        warnings: false,
+        errors: false,
+      },
     },
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+        .rule('vue')
+        .use('vue-loader')
+        .tap((options) => {
+          return {
+            ...options,
+            compilerOptions: {
+              compatConfig: {
+                MODE: 2
+              }
+            }
+          }
+        })
   },
 }
