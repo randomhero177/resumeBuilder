eac1cbcc6169de7ddf8eb74e9be51db480dd06a3
randomhero177 <randomhero177@gmail.com>
2026-03-09 23:17:51 +0100

    [#vue3]: update fontawesome, pt.3

--- frontend_vite/src/main.ts
+++ frontend_vite/src/main.ts
@@ -4,9 +4,11 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 import i18n from '@/plugins/i18n'
+import registerFontAwesome from '@/plugins/fontAwesome.ts';
 
 const app = createApp(App)
 app.use(router);
 app.use(store);
 app.use(i18n);
+registerFontAwesome(app);
 app.mount('#app')
